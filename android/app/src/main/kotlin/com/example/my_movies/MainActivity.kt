972dd8aec55b7020078e7d6ff01a1fc664c48f65
Zedequias Santos <zedequias@chatbotmaker.io>
2022-02-19 21:07:17 -0300

    .gitignore is now working

--- android/app/src/main/kotlin/com/example/my_movies/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.my_movies
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/api_result.dart
+++
@@ -1,7 +0,0 @@
-class ApiResult<T> {
-  final bool success;
-  final T? data;
-  final String? error;
-
-  ApiResult({this.success = false, this.data, this.error});
-}
